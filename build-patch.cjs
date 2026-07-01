6518caf7491232b70a135e318f2888eaf86b2558
Lowen <lowenhsu.tw@gmail.com>
2026-07-01 14:52:25 +0800

    Add build patch loader

---
+++ build-patch.cjs
@@ -0,0 +1,15 @@
+const fs = require('node:fs');
+
+function patchFile(file) {
+  if (!fs.existsSync(file)) return;
+  let text = fs.readFileSync(file, 'utf8');
+  text = text.replace(/,\"咖啡\"/g, '');
+  text = text.replace(/, \"咖啡\"/g, '');
+  if (file === 'index.html' && !text.includes('modal-ui-patch.js')) {
+    text = text.replace('</body>', '<script src="/modal-ui-patch.js"></script></body>');
+  }
+  fs.writeFileSync(file, text);
+}
+
+patchFile('index.html');
+patchFile('api/sync-products-from-drive.ts');
